fc0616fae67282bfdaae44a648e9d112ffaa2528
RadoslavDimitrovMG <radoslavddimtrov@mail.bg>
2022-06-10 22:30:46 +0300

    UnitTest Movie User

---
+++ NUnitTests/MovieContextTest.cs
@@ -0,0 +1,88 @@
+﻿using BusinessLayer;
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    class MovieContextTest
+    {
+        private MovieLibraryContext dbContext;
+        private MovieContext movieContext;
+        DbContextOptionsBuilder builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            dbContext = new MovieLibraryContext(builder.Options);
+            movieContext = new MovieContext(dbContext);
+        }
+
+        [Test]
+        public void TestCreateMovie()
+        {
+            List<Genre> genres = new List<Genre>() { new Genre("Sci-fi"), new Genre("Action"), new Genre("Thriller") };
+            
+            int moviesBefore = movieContext.ReadAll().Count();
+
+            movieContext.Create(new Movie("Star Wars", 1974, "The most epic sci-fi saga of all time", 168, genres));
+
+            int moviesAfter = movieContext.ReadAll().Count();
+
+            Assert.IsTrue(moviesBefore != moviesAfter);
+        }
+
+        [Test]
+        public void TestReadMovie()
+        {
+            List<Genre> genres = new List<Genre>() { new Genre("Sci-fi"), new Genre("Action"), new Genre("Thriller") };
+            movieContext.Create(new Movie("Star Wars", 1974, "The most epic sci-fi saga of all time", 168, genres));
+
+            Movie movie = movieContext.Read(1);
+
+            Assert.That(movie != null, "There is no record with id 1!");
+        }
+
+        [Test]
+        public void TestUpdateMovie()
+        {
+            List<Genre> genres = new List<Genre>() { new Genre("Sci-fi"), new Genre("Action"), new Genre("Thriller") };
+            movieContext.Create(new Movie("Star Wars", 1974, "The most epic sci-fi saga of all time", 168, genres));
+
+            Movie movie = movieContext.Read(1);
+
+            movie.Name = "Star Wars2";
+            movie.Genres = new List<Genre>() { new Genre("Comedy"), new Genre("Action"), new Genre("Thriller") };
+
+
+            movieContext.Update(movie);
+
+            Movie movieNew = movieContext.Read(1);
+
+            Assert.IsTrue(movieNew.Name == "Star Wars2" && movieNew.Genres.First().Name == "Comedy", "Movie Update() does not change!");
+        }
+
+        [Test]
+        public void TestDeleteMovie()
+        {
+            List<Genre> genres = new List<Genre>() { new Genre("Sci-fi"), new Genre("Action"), new Genre("Thriller") };
+            movieContext.Create(new Movie("Star Wars", 1974, "The most epic sci-fi saga of all time", 168, genres));
+
+            int moviesBeforeDelete = movieContext.ReadAll().Count();
+
+            movieContext.Delete(1);
+
+            int moviesAfterDelete = movieContext.ReadAll().Count();
+
+            Assert.AreNotEqual(moviesAfterDelete, moviesBeforeDelete);
+        }
+    }
+}
---
+++ NUnitTests/UserContextText.cs
@@ -0,0 +1,82 @@
+﻿using BusinessLayer;
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    class UserContextText
+    {
+        private MovieLibraryContext dbContext;
+        private UserContext userContext;
+        DbContextOptionsBuilder builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            dbContext = new MovieLibraryContext(builder.Options);
+            userContext = new UserContext(dbContext);
+        }
+
+        [Test]
+        public void TestCreateUser()
+        {
+            int usersBefore = userContext.ReadAll().Count();
+
+            userContext.Create(new User("Rady", 18, "Bulgaria"));
+
+            int usersAfter = userContext.ReadAll().Count();
+
+            Assert.IsTrue(usersBefore != usersAfter);
+        }
+
+        [Test]
+        public void TestReadUser()
+        {
+            userContext.Create(new User("Rady", 18, "Bulgaria"));
+
+            User user = userContext.Read(1);
+
+            Assert.That(user != null, "There is no record with id 1!");
+        }
+
+        [Test]
+        public void TestUpdateUser()
+        {
+            userContext.Create(new User("Rady", 18, "Bulgaria"));
+
+            User user = userContext.Read(1);
+
+            user.Name = "Monika";
+            user.Age = 19;
+
+            userContext.Update(user);
+
+            User userNew = userContext.Read(1);
+
+            Assert.IsTrue(userNew.Name == "Monika" && userNew.Age == 19, "User Update() does not change!");
+        }
+
+        [Test]
+        public void TestDeleteUser()
+        {
+            userContext.Create(new User("Rady", 18, "Bulgaria"));
+
+            int usersBeforeDelete = userContext.ReadAll().Count();
+
+            userContext.Delete(1);
+
+            int usersAfterDelete = userContext.ReadAll().Count();
+
+            Assert.AreNotEqual(usersBeforeDelete, usersAfterDelete);
+        }
+    }
+}
