b056b613c349a1312cce18fbb4724848416b80f2
RadoslavDimitrovMG <radoslavddimtrov@mail.bg>
2022-06-18 13:00:38 +0300

    User Form

---
+++ PresentationLayer/UserForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace PresentationLayer
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.numAge = new System.Windows.Forms.NumericUpDown();
+            this.dataGridViewUser = new System.Windows.Forms.DataGridView();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(68, 210);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 23);
+            this.txtCountry.TabIndex = 1;
+            // 
+            // numAge
+            // 
+            this.numAge.Location = new System.Drawing.Point(68, 158);
+            this.numAge.Name = "numAge";
+            this.numAge.Size = new System.Drawing.Size(120, 23);
+            this.numAge.TabIndex = 2;
+            // 
+            // dataGridViewUser
+            // 
+            this.dataGridViewUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUser.Location = new System.Drawing.Point(448, 92);
+            this.dataGridViewUser.Name = "dataGridViewUser";
+            this.dataGridViewUser.RowTemplate.Height = 25;
+            this.dataGridViewUser.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewUser.TabIndex = 3;
+            this.dataGridViewUser.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewUser_CellContentClick);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(68, 307);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 4;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(209, 306);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnDelete.Location = new System.Drawing.Point(341, 306);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Country";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(612, 306);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(68, 101);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 23);
+            this.txtName.TabIndex = 11;
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.dataGridViewUser);
+            this.Controls.Add(this.numAge);
+            this.Controls.Add(this.txtCountry);
+            this.Name = "UserForm";
+            this.Text = "UserForm";
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.NumericUpDown numAge;
+        private System.Windows.Forms.DataGridView dataGridViewUser;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
---
+++ PresentationLayer/UserForm.cs
@@ -0,0 +1,203 @@
+﻿using BusinessLayer;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class UserForm : Form
+    {
+        private MovieLibraryContext dbContext;
+        private UserContext userContext;
+        private User selectedUser;
+        private List<User> users;
+        int selectedRowIndex = -1;
+        public UserForm()
+        {
+            InitializeComponent();
+            dbContext = new MovieLibraryContext();
+            userContext = new UserContext(dbContext);
+
+            LoadHeaderRow();
+            LoadUsers();
+        }
+        private bool ValidateData()
+        {
+            if (txtName.Text != string.Empty)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void ClearData()
+        {
+            txtName.Text = string.Empty;
+
+            selectedUser = null;
+            selectedRowIndex = -1;
+        }
+      
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectedUser != null)
+                {
+                    MessageBox.Show("You can't create duplicated users!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (ValidateData())
+                {
+                    string name = txtName.Text;
+                    int age = (int)numAge.Value;
+                    string country = txtCountry.Text;
+                    User user = new User(name, age, country);
+
+                    userContext.Create(user);
+                    MessageBox.Show("User was created successfully!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    CreateRowRefresh(user);
+                    ClearData();
+                }
+                else
+                {
+                    MessageBox.Show("Name is required!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Age is required!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Country is required!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void LoadHeaderRow()
+        {
+            dataGridViewUser.Columns.Add("id", "ID");
+            dataGridViewUser.Columns.Add("name", "Name");
+            dataGridViewUser.Columns.Add("age", "Age");
+            dataGridViewUser.Columns.Add("country", "Country");            
+        }
+
+        private void LoadUsers()
+        {
+            users = userContext.ReadAll().ToList();
+
+            foreach (User item in users)
+            {
+                DataGridViewRow row = (DataGridViewRow)dataGridViewUser.Rows[0].Clone();
+
+                row.Cells[0].Value = item.ID;
+                row.Cells[1].Value = item.Name;
+                row.Cells[2].Value = item.Age;
+                row.Cells[3].Value = item.Country;
+
+                /*if (item.Movies != null)
+                {
+                    row.Cells[2].Value = string.Join(", ", item.Movies.Select(c => c.Name));
+                }*/
+
+                dataGridViewUser.Rows.Add(row);
+            }
+        }
+        private void CreateRowRefresh(User item)
+        {
+            DataGridViewRow row = (DataGridViewRow)dataGridViewUser.Rows[0].Clone();
+
+            row.Cells[0].Value = item.ID;
+            row.Cells[1].Value = item.Name;
+            row.Cells[2].Value = item.Age;
+            row.Cells[3].Value = item.Country;
+
+            /*if (item.Movies != null)
+            {
+                row.Cells[2].Value = string.Join(", ", item.Movies);
+            }*/
+
+            dataGridViewUser.Rows.Add(row);
+        }
+
+        private void UpdateRowRefresh()
+        {
+            dataGridViewUser.Rows[selectedRowIndex].Cells[0].Value = selectedUser.ID;
+            dataGridViewUser.Rows[selectedRowIndex].Cells[1].Value = selectedUser.Name;
+            dataGridViewUser.Rows[selectedRowIndex].Cells[1].Value = selectedUser.Age;
+            dataGridViewUser.Rows[selectedRowIndex].Cells[1].Value = selectedUser.Country;
+        }
+
+        private void DeleteRowRefresh()
+        {
+            dataGridViewUser.Rows.RemoveAt(selectedRowIndex);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (ValidateData() && selectedUser != null)
+            {
+                selectedUser.Name = txtName.Text;
+                selectedUser.Age = (int)numAge.Value;
+                selectedUser.Country = txtCountry.Text;
+                userContext.Update(selectedUser);
+
+                MessageBox.Show("User was updated successfully", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                UpdateRowRefresh();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Name, age and country are required!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);                
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (selectedUser != null)
+            {
+                userContext.Delete(selectedUser.ID);
+
+                DeleteRowRefresh();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("You must select a user!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dataGridViewUser_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1 && e.ColumnIndex != -1)
+            {
+                int id = Convert.ToInt32(dataGridViewUser.Rows[e.RowIndex].Cells[0].Value);
+                string name = dataGridViewUser.Rows[e.RowIndex].Cells[1].Value.ToString();
+                int age  = (int)dataGridViewUser.Rows[e.RowIndex].Cells[2].Value;
+                string country = dataGridViewUser.Rows[e.RowIndex].Cells[3].Value.ToString();
+
+                selectedUser = users.First(u => u.ID == id);
+
+                txtName.Text = name;
+                numAge.Value = age;
+                txtCountry.Text = country;
+
+                selectedRowIndex = e.RowIndex;
+            }
+        }
+    }
+}
